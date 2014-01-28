2a37f882809568e1bb5bde514e3f1243749d3fbe
Ryan Brandt <Ryabrandt@gmail.com>
2014-01-27 17:52:37 -0800

    first steps of auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Ritly::Application.routes.draw do
+  resources :users, :sessions
+
   root to: "urls#index"
+  get 'signup', to: "users#new"
+  delete 'signout', to: 'sessions#destroy'
+  get 'signin', to: 'sessions#new'
+
   get "/about", to: "urls#about"
   post "/shorten", to: "urls#create"
   get "/show", to: "urls#show"
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124191203) do
+ActiveRecord::Schema.define(version: 20140128012344) do
 
   create_table "urls", force: true do |t|
     t.string   "link"
     t.string   "random_string"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
