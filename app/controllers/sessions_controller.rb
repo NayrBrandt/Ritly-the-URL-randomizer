28d1efe846b7d94af07279d802b42deb917966fb
Ryan Brandt <Ryabrandt@gmail.com>
2014-01-27 19:24:13 -0800

    things

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,12 @@ class SessionsController < ApplicationController
   
 	def create
 	  signin_params = params[:session]
-	  user=User.find_by_email(signin_params[:email].downcase)
-	  if user && user.authenticate(signin_params[:password])
+	  
+	  user=User.find_by_email(params[:session][:email])
+	  
+	  if user && user.authenticate(signin_params[:session][:password])
 		# Sign the user in and redirect to the user's show page.
-		# sign_in user
+		sign_in user
 		redirect_to user
 	  else
 		# Create an error message and re-render the signin form.
@@ -19,7 +21,7 @@ class SessionsController < ApplicationController
   
 
   def destroy
-  	# sign_out
+  	sign_out
   	redirect_to root_url
   end
 end
